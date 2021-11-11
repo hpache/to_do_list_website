884257c7f5ddf235a7ad1c79cc9be236da913ba6
hpache <shenryp78@gmail.com>
2021-11-11 14:47:03 -0500

    Added the first part of the layout for the website

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-    return render_template("index.html")
+    return render_template("todo.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
