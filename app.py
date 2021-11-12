8cbbe652f52adec4308592bc8c9c6cd589cbb5ef
hpache <shenryp78@gmail.com>
2021-11-12 15:53:13 -0500

    Switched to SQLAlchemy

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from models import db, InfoModel
 import psycopg2
 import os
 
 app = Flask(__name__)
-DATABASE_URL = os.environ['postgres://thdubczkubqtdu:11b1f97cf5af521451340de904ba9ce1a6d4ae1d1b04bb6fc0026a0a779d1b77@ec2-54-160-103-135.compute-1.amazonaws.com:5432/d50g0qitf4h4pp']
-conn = psycopg2.connect(DATABASE_URL, sslmode = 'require')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://thdubczkubqtdu:11b1f97cf5af521451340de904ba9ce1a6d4ae1d1b04bb6fc0026a0a779d1b77@ec2-54-160-103-135.compute-1.amazonaws.com:5432/d50g0qitf4h4pp"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app,db)
 
 @app.route("/")
-
 def index():
     return render_template("todo.html")
 
