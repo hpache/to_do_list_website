af263cad3754d16fad91572c5fb156ec3199e6a7
hpache <shenryp78@gmail.com>
2021-11-12 16:02:43 -0500

    Fixed database url

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from models import db, InfoModel
-import psycopg2
-import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://thdubczkubqtdu:11b1f97cf5af521451340de904ba9ce1a6d4ae1d1b04bb6fc0026a0a779d1b77@ec2-54-160-103-135.compute-1.amazonaws.com:5432/d50g0qitf4h4pp"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://thdubczkubqtdu:11b1f97cf5af521451340de904ba9ce1a6d4ae1d1b04bb6fc0026a0a779d1b77@ec2-54-160-103-135.compute-1.amazonaws.com:5432/d50g0qitf4h4pp"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
