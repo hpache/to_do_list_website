d6ceb03d8c3fa15946c755f5ad29f79e2179627f
hpache <shenryp78@gmail.com>
2021-11-14 12:38:14 -0500

    closed #1

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from models import db, InfoModel
+from models import db, TodoModel
+from datetime import date
 
 app = Flask(__name__)
 
@@ -13,7 +14,33 @@ migrate = Migrate(app,db)
 
 @app.route("/")
 def index():
-    return render_template("todo.html")
+    completeTodos = TodoModel.query.filter_by(completed = True)
+    incompleteTodos = TodoModel.query.filter_by(completed = False)
+    return render_template("todo.html", incomplete=incompleteTodos, complete = completeTodos)
+
+@app.route("/add", methods = ["POST"])
+def add():
+    item = request.form["todoItem"]
+    currentDate = date.today()
+    priority = request.form["priority"]
+    tag = request.form["tag"]
+    item = TodoModel(item=item, date=currentDate, priority=priority, tag=tag)
+    db.session.add(item)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route("/remove", methods = ["POST"])
+def update():
+    db.session.query(TodoModel).filter_by(id = request.form["remove"]).delete()
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/complete", methods = ["POST"])
+def complete():
+    todo = TodoModel.query.filter_by(id = request.form["complete"]).first()
+    todo.completed = True 
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
