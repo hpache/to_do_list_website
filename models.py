8b41fdd7547f5d679cc0f6bb269d5d0f567c5c63
hpache <shenryp78@gmail.com>
2021-11-14 12:38:47 -0500

    Added completed item to database

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class InfoModel(db.Model):
+class TodoModel(db.Model):
 
     __tablename__ = "info_table"
 
@@ -12,13 +12,15 @@ class InfoModel(db.Model):
     date = db.Column(db.String())
     priority = db.Column(db.String())
     tag = db.Column(db.String())
+    completed = db.Column(db.Boolean)
 
 
-    def __init__(self, item, date, priority, tag) -> None:
+    def __init__(self, item, date, priority, tag, completed = False) -> None:
         self.item = item
         self.date = date
         self.priority = priority
         self.tag = tag
+        self.completed = completed
     
     def __repr__(self) -> str:
-        return f"{self.item}:{self.date, self.priority, self.tag}"
+        return f"{self.item}:{self.date, self.priority, self.tag, self.completed}"
